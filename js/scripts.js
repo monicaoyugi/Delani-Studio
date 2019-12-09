7686bf6113884cd79bb886744211b76695ce7d30
monicaoyugi <monicaoyugi@gmail.com>
2019-12-09 17:27:38 +0300

    final commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
     //toggle at design,development and product mgmt
     $(".design").click(function(){
       $(".show").toggle();
@@ -12,6 +13,8 @@ $(document).ready(function(){
       $(".show2").toggle();
       $(".click2").toggle();
     });
+
+
   
     //display white box on hover
     $(".project").hover(function(){
@@ -23,29 +26,48 @@ $(document).ready(function(){
   
     $(".button").click(function(event){
       event.preventDefault();
-      var name = document.getElementById('name').value;
-      var email = document.getElementById('email').value;
-      var message = document.getElementById('message').value;
+      var name = document.getElementById('mce-NAME').value;
+      var email = document.getElementById('mce-EMAIL').value;
+      var message = document.getElementById('mce-MESSAGE').value;
       if(name == ""){
-        alert("Please fill the name field");
+        alert("Please enter your name");
         name.focus();
         return false;
       }
       if(email == ""){
-        alert("Please fill the email field");
+        alert("Please enter your email");
         email.focus();
         return false;
       }
       if(message == ""){
-        alert("Please fill the message field");
+        alert("Please enter the required message");
         message.focus();
         return false;
       }
-      alert(name +", we have received your message. Thank you for contacting us.");
+     alert(name +", we have received your message. Thank you for contacting us.");
+    
     });
-    $("button").click(function(event){
+    $("button").click((event) => {
       $('form').each(function(){
         this.reset();
       });
     });
+
+    // function postToMailChimp(name, email, message) {
+    
+    //     $.ajax({
+    //         url: "https://gmail.us4.list-manage.com/subscribe/post?u=a27ed764514f6345683756895&id=d2f822424a",
+    //         type: "POST",
+    //         contentType: 'application/json',
+    //         data: JSON.stringify({name: name, email: email, message: message}),
+    //         processData: false,
+    //         success: function(data){
+    //           console.log(data)
+
+    //         },
+    //         error: function(errorThrown){
+    //           console.log(errorThrown)
+    //         }
+    //     })
+    // }
   });
